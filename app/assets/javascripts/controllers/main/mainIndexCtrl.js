632fbd8681ea036dea952b7ccd78d414a3b119a4
Kelly, Roger <Roger.Kelly@sabre.com>
2014-07-25 13:07:24 -0500

    adding json api and angular api call test

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,3 +1,16 @@
-function IndexCtrl($scope) {
+function IndexCtrl($scope,$http) {
     $scope.title = 'Hola amigos!';
-}
+
+	
+	
+	$http({method: 'GET', url: '/api/mdm_models'}).
+    success(function(data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+      $scope.mdmmodels = data;
+      
+      console.log($scope.mdmmodels);
+    });
+
+
+};
--- app/controllers/api/mdm_models_controller.rb
+++ app/controllers/api/mdm_models_controller.rb
@@ -4,4 +4,9 @@ class Api::MdmModelsController < ApplicationController
     @mdm_model = MdmModel.find(params[:id])
     render :json => @mdm_model.to_json(:include => {:mdm_objects => {:include => :mdm_columns}})
   end
+  
+  def index
+    @mdm_models = MdmModel.all
+    render :json => @mdm_models.to_json(:include => {:mdm_objects => {:include => :mdm_columns}})
+  end
 end
