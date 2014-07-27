2287eadcbe7aba54164402c3d4286d45622c78e9
roger <acousticorchard@gmail.com>
2014-07-27 14:50:19 -0500

    dyanamic class

--- app/models/persistor.rb
+++ app/models/persistor.rb
@@ -4,6 +4,26 @@ class Persistor
 	def promote(model)
 	end
 	
+	#serialize an mdm_object 
+	#could be that the class is generated at runtime like AR and this is an 
+	#AR instnce or it is a hash/json
+	
+	#below NOTE! Need table created first for AR
+  klass = Class.new ActiveRecord::Base do
+    def name
+      "#{super} Doe"
+    end
+  end
+  
+  module Company
+  end
+  
+  Company.const_set "Employee", klass
+  
+  puts Company::Employee.new.name # prints "Jon Doe"
+  
+ 
+  
 	def serialize(row)
 	end
 end
