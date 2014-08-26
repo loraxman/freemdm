e750972cea19422165a4b1ad7c9d84cb530bd75c
Kelly, Roger <Roger.Kelly@sabre.com>
2014-08-26 10:46:59 -0500

    moving classpath up further

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+$CLASSPATH << ENV['ORACLE_JDBC_JAR']
+$CLASSPATH << ENV['MYSQL_JAR']
 # Initialize the Rails application.
 Rails.application.initialize!
 
-$CLASSPATH << ENV['ORACLE_JDBC_JAR']
-$CLASSPATH << ENV['MYSQL_JAR']
